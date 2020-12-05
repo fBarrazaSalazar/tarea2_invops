27cf1431cc9b13bd65e759f77be80f6c32fbd125
Felipe Barraza <f.barraza.salazar@gmail.com>
2020-12-05 00:43:38 +0000

    Escritura resultados implementado

--- tarea2.py
+++ tarea2.py
@@ -99,21 +99,60 @@ def funcion_objetivo(n,vsee,f_weight,seed):
     return np.sum([calcular_distancia(x,y,vsee)*f_weight[seed[x]][seed[y]] for x in range(len(l)) for y in range(x+1,len(l))])
 
 
-def neighborhood(n,f_weight,l,it,k):
+def neighborhood(n,f_weight,l,it,k,resultados):
     """
     Esta funcion encuentra la solucion minima dentro de k neighbors, los cuales son inicializados por un seed random 
     puestos
-    @param [n] tamaño del array, [l] array inicial, [f_weight] matriz de pesos, [it] cantidad de iteraciones, [k] cantidad de neighbors
+    @param [n] tamaño del array, [l] array inicial, [f_weight] matriz de pesos, [it] cantidad de iteraciones, [k] cantidad de neighbors,[resultados] archivo de escritura
     @return :  [seed_sol],[best_sol] seed_sol es el index de la mejor solucion encontrada, best_sol es la mejor solucion.
     """
+    print("Elementos: ", n)
+    resultados.write("Elementos: ")
+    resultados.write(str(n))
+    resultados.write("\n")
+    print("Vecindarios: ", k)
+    resultados.write("Vecindarios: ")
+    resultados.write(str(k))
+    resultados.write("\n")
+    print("Iteraciones: ", it)
+    resultados.write("Iteraciones: ")
+    resultados.write(str(it))
+    resultados.write("\n\n")
+    print()
+
     best_sol=1000000
     seed_sol= np.ones(n)
+
     for x in range(k):
+        print("VECINDARIO ", x+1)
+        resultados.write("VECINDARIO ")
+        resultados.write(str(x+1))
+        resultados.write("\n")
         seed=encontrar_seed(n)
-        [seed_local,best_sol_local]=local_search(seed,it,n,l,f_weight)
+        print("Seed: ",seed)
+        resultados.write("Seed: ")
+        resultados.write(str(seed))
+        resultados.write("\n")
+
+        print("Realizando busqueda local...")
+        resultados.write("Realizando busqueda local...\n")
+
+        [seed_local,best_sol_local]=local_search(seed,it,n,l,f_weight,resultados)
+
+        print("Seed local: ",seed_local)
+        resultados.write("Seed local: ")
+        resultados.write(str(seed_local))
+        resultados.write("\n")
+        print("Solucion local: ",best_sol_local)
+        resultados.write("Solucion local: ")
+        resultados.write(str(best_sol_local))
+        resultados.write("\n")
+
         if best_sol > best_sol_local:
             best_sol = best_sol_local
             seed_sol=seed_local
+        print()
+        resultados.write("\n")
     return seed_sol,best_sol
 
 
@@ -130,17 +169,32 @@ def stochastic_iteration(n,seed):
     seed[x],seed[y]=seed[y],seed[x]
     return seed
 
-def local_search(seed,it,n,l,f_weight):
+def local_search(seed,it,n,l,f_weight,resultados):
     """
     Hace un search iterativo random dentro del espacio de busqueda, los movimientos son swap de items dentro del array
     y se encarga de encontrar la solucion minima local.
-    @param [n] tamaño del array, [l] array inicial, [f_weight] matriz de pesos, [it] cantidad de iteraciones, [seed] index de la solucion
+    @param [n] tamaño del array, [l] array inicial, [f_weight] matriz de pesos, [it] cantidad de iteraciones, [seed] index de la solucion, [resultados] archivo de escritura
     @return :  [seed_sol],[sol_local] seed_sol es el index de la mejor solucion encontrada, sol_local es la mejor solucion local.
     """
     sol_local=1000000 
     seed_sol=seed
     for x in range(it):
-        sol=funcion_objetivo(n,valor_seed(seed,l),f_weight,seed)  
+        print("Iteracion ", x+1)
+        resultados.write("Iteracion ")
+        resultados.write(str(x+1))
+        resultados.write("\n")
+        
+        sol=funcion_objetivo(n,valor_seed(seed,l),f_weight,seed)
+
+        print("(Seed) solucion local: ", sol_local)
+        resultados.write("(Seed) solucion local: ")
+        resultados.write(str(sol_local))
+        resultados.write("\n")
+        print("(Seed) solucion encontrada: ", sol)
+        resultados.write("(Seed) solucion encontrada: ")
+        resultados.write(str(sol))
+        resultados.write("\n")
+
         if sol_local > sol:
             sol_local=sol
             seed_sol=seed
@@ -173,14 +227,26 @@ if __name__ == '__main__':
     """
 
     #config---------------------------------------------------------------------
-    it=10
-    k=5
+    it=50
+    k=20
     nombre='QAP_sko56_04_n'
     #nombre='test123.txt'
     #----------------------------------------------------------------------------
+    resultados=open("resultados.txt","w")
+
     [n,l,f_weight]=adquirir_datos(nombre)
-    [seed,vsol]=neighborhood(n,f_weight,l,it,k)
-    print("\nMejor resultado\n")
+    [seed,vsol]=neighborhood(n,f_weight,l,it,k,resultados)
+    
+    print("Mejor resultado\n")
+    resultados.write("Mejor resultado\n\n")
     print("Index solución: ",seed)
+    resultados.write("Index solución: ")
+    resultados.write(str(seed))
+    resultados.write("\n")
     print("Valor solución: ",vsol)
+    resultados.write("Valor solución: ")
+    resultados.write(str(vsol))
+    resultados.write("\n")
+
+    resultados.close()
 
